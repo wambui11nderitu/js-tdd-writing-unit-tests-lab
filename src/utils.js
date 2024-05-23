364c2095c9ee830608d7f1a35e5129437f6ba169
Wambui <nderituwambui93@gmail.com>
2024-05-24 01:49:07 +0300

    updating labs

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,13 @@
 // Your code here
+// Your code here
+function reverse(string) {
+    return string.split("").reverse().join("");
+  }
+  
+  export function isPalindrome(word) {
+    if (!/^[A-Za-z]+$/.test(word)) {
+      throw new Error("Invalid input");
+    }
+    const lowerCaseWord = word.toLowerCase();
+    return lowerCaseWord === reverse(lowerCaseWord);
+  }
